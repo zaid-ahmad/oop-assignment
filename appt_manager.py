881eabc26b8d1b50b14098f5411047ce0d740eda
danielminjaekim <danielkim4106@gmail.com>
2023-12-08 22:18:17 -0700

    Final Project Part 2 -Daniel's Functions

---
+++ appt_manager.py
@@ -0,0 +1,20 @@
+from appointment import Appointment
+
+def create_weekly_calendar():
+    calendar = []
+    days_in_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    for day in days_in_week:
+        for hour in range(9, 17):
+            appointment = Appointment("", "", 0, day, hour)
+            calendar.append(appointment)
+    return calendar
+
+def print_menu():
+    print("Jojo's Hair Salon Appointment Manager")
+    print("="*37)
+    print("1) Schedule an appointment")
+    print("2) Find appointment by name")
+    print("3) Print calendar for a specific day")
+    print("4) Cancel an appointment")
+    print("5) Exit the system")
+    input("Enter your selection: ")
